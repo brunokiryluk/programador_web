ee80d1c2951b5a3c753b899c5870de2d997f37e2
Bruno Kiryluk <brunokiryluk@gmail.com>
2019-09-26 20:54:32 -0300

    commit-ejercicio

--- jquery-events/main.js
+++ jquery-events/main.js
@@ -1,23 +1,49 @@
 $(document).ready(function () {
-    console.log('Init App')
-    // Espacio para escribir el código de nuestra aplicación
-    $('#button').on('click', handleButtonClick)
-    $(document).keydown(handleKeyDown)
-    $('ul').append('<li id="3">Item 3</li>')
-    
-  })
+  console.log('Init App')
+  // Espacio para escribir el código de nuestra aplicación
+  //$('#button').on('click', handleButtonClick)
+  //$(document).keydown(handleKeyDown)
+  //$('ul').append('<li id="3">Item 3</li>')
+  $(document).keydown(counter);
+})
+
+/*
 function handleButtonClick() {
-    console.log('hola');
+  console.log('hola');
 }
 
 
-function handleKeyDown(event){
-  if(event.which === 32){
+function handleKeyDown(event) {
+  if (event.which === 32) {
     console.log('Se presionó la barra espaciadora')
   }
 
 }
 
+*/
+var contador = 0;
+
+function counter(_event) {
+
+ // contador = $('#input').val();
+
+  if (_event.which === 13) {
+    contador++;
+    //enterEvent;
+    //var enterEvent =
+  }
+  if (_event.which == 38) {
+    contador--;
+
+  }
+  if (_event.which == 40) {
+    contador = 0;
+
+  }
+ 
+  $('#input').html(contador);
+
+}
 
 
 
